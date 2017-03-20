df48fff276ba6b9c00390418a6c5a9549721e729
Steven Mao <mbaoqi@gmail.com>
2017-03-20 18:00:49 +0800

    allocator类

--- PRJ_CPP/ConsoleApplication1/main.cpp
+++ PRJ_CPP/ConsoleApplication1/main.cpp
@@ -1,9 +1,18 @@
 // ptr
 #include <iostream>
+#include <memory>
+
+using namespace std;
+
 
 int main(int argc, char** agrv)
 {
 
+	allocator<string> alloc;
+	auto const p = alloc.allocate(10);
+
+
 	return 0;
+
 
 }
