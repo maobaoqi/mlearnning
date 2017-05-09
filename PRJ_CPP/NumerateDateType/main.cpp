1d0b9dbe6258c76bd2e4d31d04a356ea14d7850b
Steven Mao <mbaoqi@gmail.com>
2017-05-09 16:55:36 +0800

    变量强制转换: static_cast<type>

--- PRJ_CPP/NumerateDateType/main.cpp
+++ PRJ_CPP/NumerateDateType/main.cpp
@@ -15,5 +15,11 @@ int main(int argc, char** argv)
 	Finger thisThumb = DMZ;
 	std::cout << "this is a thumb:" << thisThumb << std::endl;
 
+	int val1 = static_cast<int>(7.9);
+	double val2 = static_cast<double>(25);
+
+	std::cout << val1 << std::endl;
+	std::cout << val2 << std::endl;
+
 	return 0;
 }
